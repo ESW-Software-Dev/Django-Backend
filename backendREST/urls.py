1073413940c7fb2425e486f4898096bf4f5bbbaa
Michelle Keoy <michelle.keoy@meeopp.com>
2021-11-05 16:22:42 -0400

    michelle views

--- backendREST/urls.py
+++ backendREST/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import getUserFavorites, saveUserFavorites
+from .views import getUserFavorites, saveUserFavorites, createNewItem, deleteItem, createNewUser, getUserInfo
 
 urlpatterns = [
   path('userprofile/favorites/<str:userid>', getUserFavorites), 
-  path('userprofile/newfavorite/<str:userid>', saveUserFavorites)
+  path('userprofile/newfavorite/<str:userid>', saveUserFavorites),
+  path('newitem', createNewItem),
+  path('item/<str:itemid>', deleteItem),
+  path('userprofile/createnew', createNewUser),
+  path('userprofile', getUserInfo)
+
 ]
